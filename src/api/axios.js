3340aa7a101b5f21180cec85e61e6e39b5206ec7
Ritik kumar <kumarritik5395@gmail.com>
2026-08-05 02:36:02 +0530

    Fix API base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  
 const API = axios.create({
-  baseURL: "https://library-backend-lv44.onrender.com", 
+  baseURL: "https://library-backend-lv44.onrender.com/api", 
 });
  
 API.interceptors.request.use((req) => {
